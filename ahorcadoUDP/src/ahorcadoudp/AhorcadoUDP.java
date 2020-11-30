deb65f7770b97dadcb11e00adf71eb1e9694c8e8
Is-Dani <dani.dass.do@gmail.com>
2020-11-30 11:01:19 -0400

    ahorcado con UDP

---
+++ ahorcadoUDP/src/ahorcadoudp/AhorcadoUDP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcadoudp;
+
+/**
+ *
+ * @author Dani
+ */
+public class AhorcadoUDP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
